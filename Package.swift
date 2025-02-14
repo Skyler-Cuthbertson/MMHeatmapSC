3f3499f9759558e3ba7589457ef47c301307d649
Skyler Cuthbertson <114624824+Skyler-Cuthbertson@users.noreply.github.com>
2025-02-13 23:53:50 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MMHeatmap",
     platforms: [
         .iOS(.v17)],
-    products: [
+    products: [ 
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MMHeatmap",
